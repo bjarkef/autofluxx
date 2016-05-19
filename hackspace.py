21d8d0bedd1d28614d960c9d1638b959e9edbae3
Bjarke Freund-Hansen <bjarkefh@gmail.com>
2016-05-19 18:56:39 +0200

    MCTS initial implementation

--- hackspace.py
+++ hackspace.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 from fluxx.moves import EndTurnMove
 from fluxx.gamestate import GameState
@@ -16,11 +17,21 @@ class Player:
 
 
 class TreeNode:
-	def __init__(self, gamestate, parent, children):
+	def __init__(self, gamestate, parent):
 		self.gamestate = gamestate
 		self.parent = parent
-		self.children = children
 		self.depth = self.parent.depth + 1 if self.parent != None else 0
+		self.wins = 0
+		self.visits = 0
+		self.children = None
+
+	def win_ratio(self):
+		return self.wins / self.visits
+
+	def uct(self):
+		if self.visits == 0:
+			return math.inf
+		return self.win_ratio() + 2 / math.sqrt(2) * math.sqrt((2 * math.log(self.parent.visits)) / self.visits)
 
 # Start game with four players
 def main():
@@ -29,17 +40,37 @@ def main():
 	gs = GameState(num_players)
 	print("Players: {0}".format(gs.num_players))
 	
-	
-	#rootnode = TreeNode(gs, None, {m:None for m in gs.getLegalMoves()})
-	#dfs(rootnode)
-	
-	while True:
-		transitions = playout_random_game(gs)
-		print("Moves performed: {} Winning player(s): {}".format(
-			len(transitions),
-			list(p+1 for p in transitions[-1][0].getWinningPlayers())))
+	rootnode = TreeNode(gs, None)
+	for i in range(1,5):
+		selected_node = selection(rootnode)
+		selected_node = expansion(selected_node)
 
-def playout_random_game(from_state):
+		print("Selected node:")
+		selected_node.gamestate.printState()
+	
+		transitions, winning_players = simulation(selected_node.gamestate)
+		turns = sum([1 for s,m in transitions if isinstance(m, EndTurnMove)])
+		print("Simulation: Turns: {} Winning player(s): {}".format(turns, winning_players))
+
+		win = (gs.turn in winning_players) / len(winning_players)
+		backpropagation(selected_node, win)
+
+
+def selection(node):
+	if node.children == None:
+		return node
+
+	children = node.children.values()
+	return selection(max(children, key=lambda c: c.uct()))
+
+def expansion(node):
+	node.children = {
+			move: TreeNode(node.gamestate.performMove(move), node)
+			for move in node.gamestate.getLegalMoves()}
+	return random.choice(list(node.children.values()))
+		
+
+def simulation(from_state):
 	transitions = []
 	state = from_state
 	while not state.isFinished():
@@ -49,7 +80,13 @@ def playout_random_game(from_state):
 		state = state.performMove(move)
 
 	transitions.append((state, None))
-	return transitions
+	return (transitions, list(p+1 for p in transitions[-1][0].getWinningPlayers()))
+
+def backpropagation(node, win):
+	while node != None:
+		node.visits += 1
+		node.wins += win
+		node = node.parent
 
 def dfs(node):
 	if node.depth > 700:
